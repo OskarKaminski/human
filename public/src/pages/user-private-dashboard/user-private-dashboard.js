923ecbebe66080f4416497ba10bfcf4aa2a346c7
Oskar Kaminski <oskar.kaminski@intive.com>
2016-12-04 15:40:37 +0100

    Accepting requests

--- public/src/pages/user-private-dashboard/user-private-dashboard.js
+++ public/src/pages/user-private-dashboard/user-private-dashboard.js
@@ -11,31 +11,43 @@ function userPrivateDashboardCtrl(firebase, scope) {
         this.user = user;
 
         const ref = firebase.database().ref('habit-requests/');
+        const sentRef = ref.orderByChild('sender/uid')
+            .equalTo(user.uid);
+        const receivedRef = ref.orderByChild('recipient/uid')
+            .equalTo(user.uid);
 
-        ref.orderByChild('sender/uid')
-            .equalTo(user.uid)
-            .on('value', snapshot => {
-                this.habitsSent = _(snapshot.val())
-                    .values()
-                    .value();
-                scope.$digest();
+        sentRef.on('value', snapshot => {
+            this.habitsSent = _(snapshot.val())
+                .values()
+                .value();
+        });
+        sentRef.once('value', () => {
+            scope.$digest();
+        });
+
+        receivedRef.on('value', snapshot => {
+            const values = _.map(snapshot.val(), (obj, key)=> {
+                return {
+                    ...obj,
+                    uid: key
+                }
             });
-
-        ref.orderByChild('recipient/uid')
-            .equalTo(user.uid)
-            .on('value', snapshot => {
-                this.habitsReceived = _(snapshot.val())
-                    .values()
-                    .filter(['accepted', false])
-                    .value();
-
-                this.yourCurrentHabits = _(snapshot.val())
-                    .values()
-                    .filter(['accepted', true])
-                    .value();
-                scope.$digest();
-            });
+            this.habitsReceived = _.filter(values, ['accepted', false]);
+            this.yourCurrentHabits = _.filter(values, ['accepted', true]);
+        });
+        receivedRef.once('value', () => {
+            scope.$digest();
+        });
     });
+
+    this.acceptChallenge = (challenge) => {
+        challenge.accepted = true;
+        const objToSave = _.omit(challenge, ['uid', '$$hashKey']);
+        firebase.database()
+            .ref(`/habit-requests/${challenge.uid}`)
+            .update(objToSave);
+
+    }
 
 
 }
