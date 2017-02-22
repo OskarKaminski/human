01be3fbcc71967f9edd44c31fe564e75550d4362
Oskar Kaminski <priv.oskar@gmail.com>
2017-02-22 21:09:10 +0100

    Quick fix fox error with unsubscribe error

--- public/src/pages/profile/profile.page.js
+++ public/src/pages/profile/profile.page.js
@@ -7,55 +7,55 @@ import 'rxjs/add/operator/switchMap';
 
 export class ProfilePage {
 
-    constructor(users, feedback, route) {
-        this.users = users;
-        this.feedback = feedback;
-        this.request = {};
-        this.route = route;
-    }
+  constructor (users, feedback, route) {
+    this.users = users;
+    this.feedback = feedback;
+    this.request = {};
+    this.route = route;
+  }
 
-    ngOnInit() {
-        this.profileObservable = this.route.params.switchMap(params => {
-            return this.users.find(params.id);
-        }).subscribe(user => {
-            this.user = user;
-        });
-    }
+  ngOnInit () {
+    this.profileObservable = this.route.params.switchMap(params => {
+      return this.users.find(params.id);
+    }).subscribe(user => {
+      this.user = user;
+    });
+  }
 
-    setType(type) {
-        this.request.type = type;
-    }
+  setType (type) {
+    this.request.type = type;
+  }
 
-    sendChallenge = (description) => {
+  sendChallenge = (description) => {
 
-        this.request.description = description;
+    this.request.description = description;
 
-        this.feedbackObservable = this.users.currentUser.filter(user => user)
-            .map(user => ({
-                ...this.request,
-                recipient: this.users.transformToDb(this.user),
-                sender: this.users.transformToDb(user)
-            }))
-            .subscribe(feedback => {
-                this.feedback.send(feedback);
-            });
-    }
-
-    ngOnDestroy() {
-        this.profileObservable.unsubscribe();
+    this.feedbackObservable = this.users.currentUser.filter(user => user)
+      .map(user => ({
+        ...this.request,
+        recipient: this.users.transformToDb(this.user),
+        sender: this.users.transformToDb(user)
+      }))
+      .subscribe(feedback => {
+        this.feedback.send(feedback);
         this.feedbackObservable.unsubscribe();
-    }
+      });
+  }
+
+  ngOnDestroy () {
+    this.profileObservable.unsubscribe();
+  }
 }
 
 ProfilePage.annotations = [
-    new Component({
-        selector: 'profile',
-        template
-    })
+  new Component({
+    selector: 'profile',
+    template
+  })
 ];
 
 ProfilePage.parameters = [
-    [Users],
-    [Feedback],
-    [ActivatedRoute]
+  [Users],
+  [Feedback],
+  [ActivatedRoute]
 ];
